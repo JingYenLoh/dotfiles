433e4366f7d656341a23ba49a027f474459c5f46
Jing Yen Loh <lohjingyen@gmail.com>
2018-04-01 02:40:16 +0800

    Doom Emacs initial commit
    Pretty fun distribution

---
+++ doom-emacs/.config/doom/config.el
@@ -0,0 +1,42 @@
+;;; config.el -*- lexical-binding: t; -*-
+
+;; Default settings
+(setq-default tab-width 2
+              js-indent-level 2
+              indent-tabs-mode 2)
+
+;; Set variables
+(setq doom-line-numbers-style 'relative
+
+      ;; Org settings
+      org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+        (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELED(c@/!)"))
+      ;; Tags
+      org-tag-alist (quote (("Errand"   . ?e)
+                            ("Office"   . ?o)
+                            ("Home"     . ?h)
+                            ("School"   . ?s)
+                            ("Learn"    . ?l)
+                            (:newline)
+                            ("WAITING"  . ?w)
+                            ("HOLD"     . ?H)
+                            ("CANCELED" . ?c)))
+      org-fast-tag-selection-single-key nil
+      ;; Refiling
+      org-refile-use-outline-path 'file
+      org-outline-path-complete-in-steps nil
+      org-refile-allow-creating-parent-nodes 'confirm
+      org-refile-targets '(("Next.org"     :level    . 0)
+                           ("Someday.org"  :level    . 0)
+                           ("Projects.org" :maxlevel . 1))
+      ;; Capturing
+      org-capture-templates
+      '(("i" "inbox" entry (file "~/Dropbox/org/Inbox.org")
+         "* TODO %?")
+        ("g" "Google calendar" entry (file "~/Dropbox/org/Calendar.org")
+         "* %?\n\n%^T\n\n:PROPERTIES:\n\n:END:\n\n")))
+
+;; Use fd for escaping in evil. Doom defaults to jk
+(after! evil-escape
+  (setq evil-escape-key-sequence "fd"))
